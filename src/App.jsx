553fd8172623db78e59892c385099d1232da3079
udezueoluomachi <74429592+udezueoluomachi@users.noreply.github.com>
2023-12-09 01:31:07 +0100

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import {useState} from "react";
 
+const lhsBtnsChars = [["7","8","9"],["4","5","6"],["1","2","3"],[".","0","="]]
+const rhsBtnsChars = ["DEL","/", "*", "+", "-"]
+
+const lhsBtns = lhsBtnsChars.map(
+  row => (
+    <div className="w-full h-1/4" key={row[0]}>
+      <button className="w-1/3 h-full cursor-default">{row[0]}</button>
+      <button className="w-1/3 h-full cursor-default">{row[1]}</button>
+      <button className={row[2] == "=" ? "bg-custom-orange text-custom-grey w-1/3 h-full cursor-default" : "w-1/3 h-full cursor-default"}>{row[2]}</button>
+    </div>
+  )
+)
+
+const rhsBtns = rhsBtnsChars.map(
+  row => (
+    <button className="block w-full h-1/5 cursor-default" key={rhsBtnsChars.indexOf(row)}>
+      {row}
+    </button>
+  )
+)
+
 function App() {
   const [input, setInput] = useState(0)
   const [output, setOutput] = useState("")
 
-  const lhsBtnsChars = [["7","8","9"],["4","5","6"],["1","2","3"],[".","0","="]]
-  const lhsBtnsCont = document.querySelector("#lhsBtnsCont");
 
 //bg-custom-grey
   return (
@@ -15,14 +34,12 @@ function App() {
         <p className="font-medium text-lg mt-4">{output}</p>
       </div>
       <div className="bg-custom-grey text-custom-off-white flex p-px h-[75vh] font-light text-base">
-        <div className="w-10/12 min-h-[370px] h-full" id="lhsBtnsCont">
-          <div className="w-full h-1/4">
-            <button className="w-1/3 h-full">7</button>
-            <button className="w-1/3 h-full">8</button>
-            <button className="w-1/3 h-full">9</button>
-          </div>
+        <div className="w-3/4 min-h-[370px] h-full">
+          {lhsBtns}
         </div>
-        <div className="w-2/12 min-h-[370px] h-full"></div>
+        <div className="w-1/4 min-h-[370px] h-full text-custom-orange">
+          {rhsBtns}
+        </div>
       </div>
     </>
   )
